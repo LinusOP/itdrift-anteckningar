ea1da067031453862029e7d0bc08320e8f661ff5
LinusOP <32105232+LinusOP@users.noreply.github.com>
2023-09-02 13:50:03 +0200

    Add parsing and rendering of KaTeX

--- src/app/[course]/[fileName]/page.tsx
+++ src/app/[course]/[fileName]/page.tsx
@@ -1,5 +1,9 @@
 import { getPostFromSlug, getPostData } from "@/lib/api";
 import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import "katex/dist/katex.min.css";
 
 type PageProps = {
   params: {
@@ -17,7 +21,21 @@ export default async function Page({ params }: PageProps) {
 
   return (
     <>
-      <ReactMarkdown className="prose prose-invert mx-auto h-fitToHeader max-w-full overflow-y-auto bg-slate-800 px-5 py-3 lg:prose-lg xl:prose-xl prose-headings:my-2 prose-h1:text-center">
+      <ReactMarkdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[
+          [
+            rehypeKatex,
+            {
+              macros: { ",": "{\\char`,}" },
+              trust: true,
+              fleqn: true,
+              strict: false,
+            },
+          ],
+        ]}
+        className="prose prose-invert mx-auto h-fitToHeader max-w-full overflow-y-auto bg-slate-800 px-5 py-3 lg:prose-lg xl:prose-xl prose-headings:my-2 prose-h1:text-center"
+      >
         {content}
       </ReactMarkdown>
     </>
