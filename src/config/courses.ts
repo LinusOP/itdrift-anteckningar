671fc0b1fc0f1c419cd69a32d1cdf5255dafea2f
LinusOP <32105232+LinusOP@users.noreply.github.com>
2024-02-19 11:49:19 +0100

    Infra 2 - Föreläsning 8

--- src/config/courses.ts
+++ src/config/courses.ts
@@ -1,7 +1,7 @@
 export const courseCodes = {
   itd11e: "Introduktion till informationsteknologi",
-  itd12c: "Nätverksteknik 1",
-  itd13d: "Infrastrukturella serversystem 1",
+  itd12c: "Nätverksteknik I",
+  itd13d: "Infrastrukturella serversystem I",
   itd21c: "Nätverksteknik II",
   itd23e: "Infrastrukturella serversystem II",
   itd22e: "Molnbaserade tjänster",
